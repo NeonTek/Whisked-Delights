3d1f04e2c3da396382761d58292768fa9c595569
Wesley Weiss <neontek6@gmail.com>
2025-09-20 09:27:42 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const hamburger = document.getElementById("hamburger-icon");
+const mobileMenu = document.getElementById("mobile-menu-container");
+
+hamburger.addEventListener("click", () => {
+  // Toggle the 'open' class on the hamburger icon for the 'X' animation
+  hamburger.classList.toggle("open");
+
+  // Toggle the 'show' class on the menu to make it appear/disappear
+  mobileMenu.classList.toggle("show");
+});
