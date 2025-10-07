18c95cf8fd4d9d9639200f1aea3acc46c33f3720
Wesley Weiss <neontek6@gmail.com>
2025-10-07 10:43:09 +0300

    Adding preloader to about page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
 $(document).ready(function () {
-  // Fade out preloader after 4s
-  setTimeout(function () {
-    $("#preloader").fadeOut("slow", function () {
-      $(this).remove(); // remove after fadeOut completes
-    });
-  }, 5000);
-
   // Hamburger menu toggle
   $("#hamburger-icon").on("click", function () {
     $(this).toggleClass("open"); // animate hamburger to X
